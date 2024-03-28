a11d74978d2fe89e46cf56587e536309f1547e93
Yogita Shete <yogitashete24@gmail.com>
2024-03-28 21:29:45 +0530

    datatype and typecasting

--- 02_variables/index.py
+++ 02_variables/index.py
@@ -2,4 +2,12 @@ student_name="Shri"
 print(student_name + "is a Good Boy")
 
 failed_subject="3"
-print('Your son' + student_name + 'failed in' + failed_subject + 'subjects')
+print('Your son ' + student_name + ' failed in ' + failed_subject + ' subjects')
+
+
+# datatypes and typecasting
+
+print(type('hello'))
+print(type(4))
+print(type(False))
+
