bcf5f24acd60a5f03cde539222efea2187517b1d
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 17:04:08 +0530

    Demonstrate adding values to lists using append() and insert() methods

---
+++ 07_Methods/adding_in_list.py
@@ -0,0 +1,13 @@
+# We can add value in using append() and insert method
+
+
+girlName = ["Kirti", "Ira", "Saisha", "Taniksha"]
+print(girlName)
+girlName.append("Shrisha")
+print(girlName)
+
+
+boyName = ["Irashya", "Kailarya", "Devanshya"]
+print(boyName)
+boyName.insert(2, "Aaradhya")
+print(boyName)
