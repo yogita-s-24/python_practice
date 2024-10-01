6db1b5a3922ef784fe910e4ab5e8245e96ea3021
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 16:42:31 +0530

    multiple assignment value to the variable example

---
+++ 05_List/multiple_assignment.py
@@ -0,0 +1,13 @@
+cat = ['fat', 'gray', 'loud']
+size = cat[0]
+color = cat[1]
+disposition = cat[2]
+
+print(size, color, disposition)
+
+
+cats = ['fatty', 'gracy', 'loudly']
+size, color, disposition = cats
+
+print(cats)
+
