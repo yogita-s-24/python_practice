91852ea29b735ef6481c571d4394da60aac2b24e
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 18:35:16 +0530

    Add dictionary comparison and conversion to list example

---
+++ 09_Dictionary/index.py
@@ -0,0 +1,10 @@
+eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+print(eggs)
+print(eggs.keys)
+ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+print(eggs == ham)
+
+
+# Convert dic into list
+eggs1 = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+print(list(eggs1))
