897ab1b864bd8426b0cf0a330ab78bfd522c0340
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 16:20:43 +0530

    Implement student name check to verify presence in students list

---
+++ 05_List/in.py
@@ -0,0 +1,8 @@
+students = ["yogi", "sonu", "didi"]
+print(students)
+
+name = input()
+if  name in students:
+    print(name+ " is in list.")
+else:
+    print("Not find " + name + " in the list.")
