32a531a64e8986fdc4aebf09f578400c88241a50
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 13:17:16 +0530

    Add arithmetic operations: addition, subtraction, multiplication, division, modulus, and exponentiation

---
+++ 03_arithmetic_oper/index.py
@@ -0,0 +1,9 @@
+a=9
+b=2
+print('Addition : ', a + b)
+print('Subtraction : ', a - b)
+print('Multiplication : ', a * b)
+print('Division (float) : ', a / b)
+print('Division (floor) : ', a // b)
+print('Modulus : ', a % b)
+print('Exponent : ', a ** b)
