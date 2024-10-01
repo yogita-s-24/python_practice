94d6e6182d3da9aea7a93871119bd1881aa82bdf
Yogita Shete <yogitashete24@gmail.com>
2024-10-01 16:52:27 +0530

    Demonstrate variable manipulation and string repetition in Python

---
+++ 06_Assignment_operator/operator.py
@@ -0,0 +1,18 @@
+spam = 42
+spam = spam + 1
+print(spam)
+
+
+spams = "Hello"
+spams += " world"
+print(spams)
+
+
+spam1 = 3
+spam1 *= 2
+print(spam1)
+
+
+spam2 = "Yogita"
+spam2 *= 4
+print(spam2)
