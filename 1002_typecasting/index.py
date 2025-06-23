f2d21472006a9359250f3d5101fd44273af8d75e
Yogita Shete <yogitashete24@gmail.com>
2025-06-23 09:58:44 +0530

    Add type casting examples for int, float, and complex types with default and non-default values

---
+++ 1002_typecasting/index.py
@@ -0,0 +1,58 @@
+# type casting : process of converting / changing from one data type to another
+
+# integers
+
+a = 10  # non-default value
+b = 0  # default value
+
+# integers to float
+print(float(a))
+print(float(b))
+
+# int to complex
+print(complex(a))
+print(complex(b))
+
+# int to bool
+print(bool(a))
+print(bool(b))
+
+# integers can be converted to all other individual data types
+
+###################################################################
+# floats
+
+a = 1.5  # non-default value
+b = 0.0  # default
+
+# float to int
+print(int(a))
+print(int(b))
+
+# float to complex
+print(complex(a))
+print(complex(b))
+
+# float to bool
+print(bool(a))
+print(bool(b))
+
+# floats can be converted to all other individual data types
+
+#################################################################
+# complex
+
+a = 1 + 2j  # non-default value
+b = 0j     # default value
+
+# complex to int
+# print(int(a))   # TypeError
+
+# complex to float
+# print(float(a))
+
+# complex to bool
+print(bool(a))
+
+# complex can be converted only into boolean type
+
