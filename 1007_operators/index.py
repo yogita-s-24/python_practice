1fe1f8b61b281f914fbf508717dde49a3e38d7a3
Yogita Shete <yogitashete24@gmail.com>
2025-07-02 21:18:32 +0530

    Add examples for character case conversion, logical, bitwise, identity, and membership operators

--- 1007_operators/index.py
+++ 1007_operators/index.py
@@ -55,3 +55,92 @@ print(char.upper())  # inbuilt method
 # upper to lower
 char = "D"
 print(char.lower())  # inbuilt method
+
+
+# using upper to lower 
+ascii = ord(char)
+print(ascii)
+print(ascii + 32)
+print(chr(ascii + 32))
+
+#using lower to upper
+char = "d"
+ascii = ord(char)
+print(ascii - 32)
+print(chr(ascii - 32))
+
+# upper to lower
+char = "Z"
+ascii = ord(char)
+print(ascii + 32)
+print(chr(ascii + 32))
+
+############################################################
+# logical operators
+
+a = 10
+b = 2
+c = 4
+
+# and 
+ 
+print( a > b and a > c)         # T T = True
+print(b > a and c > a)          # F F = False
+print(c > a and c > b)          # F T = False
+
+# or 
+print( a > b or a > c)         # T T = True
+print(b > a or c > a)          # F F = False
+print(c > a or c > b)          # F T = True
+
+# not 
+print(not(a > b))          # False
+print(not(b > c))          # True
+
+
+
+###########################################################
+
+# Perform bitwise operator
+
+# identity Operators : is, is not
+
+a = 10 
+b = 10 
+
+print( a == b)      # True
+print(id(a))
+print(id(b))
+
+print(id(a) == id(b))
+
+print( a is b)           #True
+print( a is not b)      # False
+
+x = [1,2,3,4,4,5,3]
+print(x is 4)   #false
+
+l = [10, 20, 30]
+x = [10, 20, 30]
+
+print(l is x)      # False
+print(l == x)      # True
+
+print( l is not x)   #True
+
+#############################################################
+# membership operators : in, not in 
+
+t = (10,20,30,40,50,60)
+
+print(10 in t)          #True
+print(100 not in t)     #True
+
+t = (10, 20, 30, 40, 50)
+print(2, 4 + 3)
+print(12, 10 in t)
+
+print(89, 10, 30 in t)
+
+
+
